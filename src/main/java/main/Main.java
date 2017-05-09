a2084fec1b5c4cf8f1f124bb0e1ee010c70a3d20
Michael-Sui <suijiyuan1996@gmail.com>
2017-05-09 22:09:39 +0800

    Initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by Michael on 2017/5/9.
+ */
+public class Main {
+    private static Logger LOG = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        
+    }
+}
